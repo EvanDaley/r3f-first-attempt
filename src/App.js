a4cdcb0ae2289e64693fd225dd408eeb0d3555a5
EvanDaley <daley.evan@gmail.com>
2021-07-13 16:40:14 -0700

    Adding static assets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import './App.css';
 import Box from './components/Box';
-import { OrbitControls, Stats } from '@react-three/drei';
+import Robot from './components/Robot';
+import Stork from './Stork'
+import { OrbitControls, Stats, Html } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react'
 
 function App() {
-
-
   return (
     <>
-    <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <OrbitControls />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-      <Box position={[-1.2, 2.2, 0]} />
-      <Box position={[1.2, 2.2, 0]} />
-      <Stats />
-    </Canvas>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <OrbitControls />
+        <Suspense fallback={<Html>loading..</Html>}>
+          <Stork/>
+        </Suspense>
+
+        <Box position={[1.2, 0, 0]} />
+        <Box position={[-1.2, 2.2, 0]} />
+        <Box position={[1.2, 2.2, 0]} />
+        <Stats />
+      </Canvas>
     </>
   );
 }
