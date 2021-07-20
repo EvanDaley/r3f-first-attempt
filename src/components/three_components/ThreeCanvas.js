4dcb0812187d0e642c1661c6ec6fd6e26407a4ff
Evan Daley <daley.evan@gmail.com>
2021-07-19 20:03:00 -0700

    Adding an abstract shape

--- src/components/three_components/ThreeCanvas.js
+++ src/components/three_components/ThreeCanvas.js
@@ -1,4 +1,5 @@
 import LinesRobot from './objects/LinesRobot'
+import AbstractSphere from './objects/AbstractSphere'
 import { OrbitControls, Stats, Stage, Loader, PerspectiveCamera } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { Suspense } from 'react'
@@ -22,13 +23,14 @@ function App() {
       <Canvas colorManagement={true} invalidateFrameloop pixelRatio={[1, 2]}>
         <PerspectiveCamera makeDefault position={[1, 6, 10]} />
 
-        <directionalLight position={[-10, -10, -5]} intensity={1} />
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
+        {/* <directionalLight position={[-10, -10, -5]} intensity={1} /> */}
+        {/* <ambientLight /> */}
+        {/* <pointLight position={[10, 10, 10]} /> */}
 
         <Suspense fallback={null}>
           <Stage adjustCamera={false} environment={environmentOptions[0]} intensity={.5} contactShadow={true} shadows={true}>
-            <LinesRobot />
+            {/* <LinesRobot /> */}
+            <AbstractSphere/>
           </Stage>
         </Suspense>
 
