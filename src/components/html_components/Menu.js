e7ba1c4191474ceebfd4f5858d412a6f5c3fe7d2
EvanDaley <daley.evan@gmail.com>
2021-07-20 20:56:29 -0700

    Updating the about page

--- src/components/html_components/Menu.js
+++ src/components/html_components/Menu.js
@@ -4,6 +4,105 @@
 import { HamburgerButton } from "react-hamburger-button";
 import React, { useState } from 'react';
 
+function IconList() {
+  const iconwidth = '32px'
+
+  const images = [
+    {
+      src: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/vue/vue.png',
+      alt: 'Vue',
+      link: ''
+    },
+    {
+      src: 'https://raw.githubusercontent.com/github/explore/fbceb94436312b6dacde68d122a5b9c7d11f9524/topics/aws/aws.png',
+      alt: 'Amazon Web Services',
+      link: ''
+    },
+
+    {
+      src: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png',
+      alt: 'JavaScript',
+      link: ''
+    },
+    {
+      src: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png',
+      alt: 'React',
+      link: ''
+    },
+    {
+      src: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/graphql/graphql.png',
+      alt: 'GraphQL',
+      link: ''
+    },
+    {
+      src: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png',
+      alt: '',
+      link: ''
+    },
+    {
+      src: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongodb/mongodb.png',
+      alt: '',
+      link: ''
+    },
+    {
+      src: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png',
+      alt: '',
+      link: ''
+    },
+    {
+      src: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/nodejs/nodejs.png',
+      alt: '',
+      link: ''
+    },
+    {
+      src: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/sass/sass.png',
+      alt: '',
+      link: ''
+    },
+    {
+      src: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/rails/rails.png',
+      alt: '',
+      link: ''
+    },
+    {
+      src: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/postgresql/postgresql.png',
+      alt: '',
+      link: ''
+    },
+    {
+      src: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/git/git.png',
+      alt: '',
+      link: ''
+    },
+    {
+      src: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/terminal/terminal.png',
+      alt: 'Terminal',
+      link: ''
+    },
+
+
+    {
+      src: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/sql/sql.png',
+      alt: 'SQL',
+      link: ''
+    },
+
+    {
+      src: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/visual-studio-code/visual-studio-code.png',
+      alt: 'VS Code',
+      link: ''
+    },
+  ]
+
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap", width: "100%", justifyContent: "space-around", alignContent: 'space-between' }}>
+      {images.map(function (item, index) {
+        return <img key={index} align="left" alt={item.alt} width={iconwidth} src={item.src} />
+      })}
+    </div>
+  )
+}
+
 function MenuContent({ setSelection }) {
   const overlayStyle = {
     pointerEvents: "auto",
@@ -24,7 +123,8 @@ function MenuContent({ setSelection }) {
     width: "100%",
     textAlign: "center",
     marginTop: "48px",
-    fontSize: "40px"
+    fontSize: "40px",
+    padding: "0px"
   }
 
   const buttonStyle = {
@@ -43,7 +143,7 @@ function MenuContent({ setSelection }) {
       <div style={overlayContentStyle}>
         <ul>
           <ol style={overlayContentStyle}>
-            <button style={buttonStyle} onClick={() => setSelection('home')}>Home</button>
+            <button style={buttonStyle} onClick={() => window.location.reload()}>Home</button>
           </ol>
           <ol style={overlayContentStyle}>
             <button style={buttonStyle} onClick={() => setSelection('about-me')}>About Me</button>
@@ -67,33 +167,66 @@ function AboutMe() {
     zIndex: "1",
     top: "0",
     left: "0",
-    backgroundColor: "black",
+    backgroundImage: "linear-gradient(90deg, #134E5E 10%, #71B280 90%)",
     color: "white",
     overflowX: "hidden",
     transition: "0.1s"
   }
 
+  const imagePath = window.location.href + '/images/profile.png'
+
+  return (
+    <div style={overlayStyle}>
+      <div className='flex-container'>
+        <div className='flex-item-left'>
+          <img src={imagePath} alt="profile pic" style={{borderRadius: '8px'}}/> 
+          {/* <div style={{width: "200px"}}> */}
+            {/* <IconList /> */}
+          {/* </div> */}
+        </div>
+        <div className='flex-item-right'>
+          <p style={{ marginTop: '0px' }}>My name is Evan Daley and I'm a Lead Software Engineer at Two Barrels LLC. </p>
+          <p>I consider myself to be an experienced developer, leader and architect. My strongest proficiencies are system design, JavaScript, Vue, React, Rails, AWS, Docker, and SQL. </p>
+          <p style={{ marginBottom: '40px' }}>In my free time, I like to exercise, sleep, and dig into different technologies like R, Kubernetes, GCP, and lately, three.js. This site was built with Three.js and React. </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function GetInTouch() {
+  const overlayStyle = {
+    pointerEvents: "auto",
+    height: "100%",
+    width: "100%",
+    position: "fixed",
+    zIndex: "1",
+    top: "0",
+    left: "0",
+    color: "white",
+    overflowX: "hidden",
+    transition: "0.1s",
+    justifyContent: 'center',
+    dispay: "flex",
+    backgroundImage: "linear-gradient(90deg, #134E5E 10%, #71B280 90%)",
+  }
+
   const overlayContentStyle = {
+    justifyContent: 'center',
     display: "flex",
     position: "relative",
     top: "25%",
     width: "100%",
-    textAlign: "left",
+    textAlign: "center",
     marginTop: "48px",
-    fontSize: "25px",
+    fontSize: "35px",
   }
 
   return (
     <div style={overlayStyle}>
       <div style={overlayContentStyle}>
-        <div style={{ width: "30%", display: "inline", textAlign: "right", marginRight: "20px" }}>
-          <p>[Image]</p>
-        </div>
-        <div style={{ width: "50%", display: "inline" }}>
-          <p>My name is Evan Daley and I'm a Lead Software Engineer at Two Barrels LLC. </p>
-          <p>I consider myself to be an experienced developer, leader and architect. My strongest proficiencies are JavaScript, Vue, React, Rails, AWS, Docker, and SQL. </p>
-          <p>In my free time, I like to exercise, sleep, and dig into different technologies like R, Kubernetes, GCP, and three.js. Lately I've been a bit obsessed with three.js and react-three-fiber, so thats why this site exists. </p>
-        </div>
+        The best place to reach me is at<br />
+        daley.evan@gmail.com
       </div>
     </div>
   )
@@ -116,7 +249,7 @@ export default function HTMLContent() {
     width: "100%",
     padding: "40px",
     position: "fixed",
-  };
+  }
 
   return (
     <>
@@ -136,6 +269,7 @@ export default function HTMLContent() {
 
       {inMenu ? <MenuContent setSelection={setSelection} /> : null}
       {selection === 'about-me' ? <AboutMe /> : null}
+      {selection === 'get-in-touch' ? <GetInTouch /> : null}
     </>
   )
 }
