4da9509a11fb1652411641fab9e0d6625e24c7b9
EvanDaley <daley.evan@gmail.com>
2021-07-21 22:08:59 -0700

    Small tweak

--- src/components/html_components/HTMLContent.js
+++ src/components/html_components/HTMLContent.js
@@ -5,28 +5,9 @@
 import Menu from './Menu'
 
 export default function HTMLContent({ sceneIndex }) {
-  const htmlStyle = {
-    height: '100%',
-    width: '100%',
-    zIndex: -1000,
-    position: 'absolute',
-    pointerEvents: 'none'
-  }
-
-  const sceneBackgrounds = [
-    'linear-gradient(90deg, #134E5E 10%, #71B280 90%)',
-    'linear-gradient(90deg, #56135e 10%, #b3708d 90%)'
-  ]
-
   return (
-    <div className="html-content-container" style={
-      {
-        ...htmlStyle,
-        backgroundImage: sceneBackgrounds[sceneIndex]
-      }
-    }>
-      <Menu/>
-
+    <div className="html-content-container">
+      <Menu />
     </div>
   )
 }
